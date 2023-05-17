9979b4618f4dc2501dbd49e2b9fa23812d319e3d
Blake Reece <frankiereece3@gmail.com>
2023-05-17 10:10:19 -0700

    added function for seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -9,3 +9,28 @@ useNewUrlParser: true,
 useUnifiedTopology: true,
 });
 
+const seedDB = async () => {
+    try {
+      // Clear existing data
+      await Promise.all([
+        User.deleteMany({}),
+        Thought.deleteMany({})
+      ]);
+
+        // Seed users
+        const usersEL = await User.insertMany(data.usersEL);
+
+        // Seed thoughts
+        const thoughtPromises = data.thoughts.map(async (thought) => {
+            const user = usersEL.find((user) => user.username === thought.username);
+            const newThought = new Thought({
+              thoughtText: thought.thoughtText,
+              username: thought.username,
+              userId: user._id
+            });
+            user.thoughts.push(newThought);
+            await newThought.save();
+            await user.save();
+            });
+
+            await Promise.all(thoughtPromises);
